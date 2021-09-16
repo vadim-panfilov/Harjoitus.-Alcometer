5e7c4f26791c959687079e8483d06b20f0890f6a
Vadim Panfilov <n0pava00@students.oamk.fi>
2021-09-16 19:34:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
+function App () {
+  const [alc_result, setalcresult] = useState(0)
+  const calculate = e => {
+    e.preventDefault()
+    let weight = parseInt(document.querySelector('#weight').value)
+    let litres = parseInt(document.querySelector('#bottles').value) * 0.33
+    let hours = parseInt(document.querySelector('#hours').value)
+    let grams = litres * 8 * 4.5 - (weight / 10) * hours
+    let gender = document.querySelector('#gender').value
+    let delta = 0.1337
+    if (gender === '1') {
+      delta = 0.6
+    } else {
+      delta = 0.7
+    }
+    let level = grams / (weight * delta)
+    setalcresult(level.toFixed(1))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='app-title'>
+        <h1> Calculating alcohol blood level</h1>
+      </div>
+      <form>
+        <text>Weight:</text>
+        <input type='text' id='weight' />
+        <br />
+        <br />
+        <text>Bottles:</text>
+        <input type='number' id='bottles' name='bottles' min='1' />
+        <br />
+        <br />
+        <text>Time:</text>
+        <input type='number' id='hours' name='hours' min='1' />
+        <br />
+        <br />
+        <text>Gender:</text>
+        <input type='radio' id='gender' name='gender' value='0' />
+        Male
+        <input type='radio' id='gender' name='gender' value='1' /> Female
+        <br />
+        <br />
+        <input type='text' id='result' value={alc_result} readonly='readonly' />
+        <br />
+        <br />
+        <button onClick={calculate}>Calculate</button>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
